366dc104b1cee418f72465be69b3c0aba8fa9fa0
Nikolai Merz <Nikolai Merz>
2017-12-02 10:13:40 +0100

    Projektdateien hinzufügen.

---
+++ Reiner_Autoworker/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Reiner_Autoworker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.myTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.myTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // myTable
+            // 
+            this.myTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.myTable.Location = new System.Drawing.Point(13, 82);
+            this.myTable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.myTable.Name = "myTable";
+            this.myTable.Size = new System.Drawing.Size(650, 309);
+            this.myTable.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 404);
+            this.Controls.Add(this.myTable);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.myTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView myTable;
+    }
+}
+
---
+++ Reiner_Autoworker/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace Reiner_Autoworker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            parsData();
+        }
+
+        private void parsData()
+        {
+            using (TextFieldParser parser = new TextFieldParser(@"C:\Users\nikol\Desktop\Download.csv"))
+            {
+                DataTable dataTable = new DataTable();
+
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";");
+                try
+                {
+                    string[] header = parser.ReadFields();
+                    int size = header.Length;
+                    DataColumn[] columns = new DataColumn[size];
+                    for (int i = 0; i < size; i++)
+                    {
+                        columns[i] = new DataColumn(header[i]);
+                    }
+                    dataTable.Columns.AddRange(columns);
+
+                    while (!parser.EndOfData)
+                    {
+                        //Process row
+                        string[] fields = parser.ReadFields();
+                        Object[] row = { fields[0] };
+                        dataTable.Rows.Add(fields);
+                        //foreach (string field in fields)
+                        //{
+                        //TODO: Process field
+                        //}
+                    }
+                    myTable.DataSource = dataTable;
+
+                }
+                catch
+                {
+                    MessageBox.Show(new Form() { TopMost = true }, "Ups, hier ist etwas schief gelaufen:\nDie gewählte Datei scheint nicht korrekt formatiert zu sein. Bitte korrekte Paypal Datei auswählen!");
+                }
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            myTable.Size = new Size (control.Width-40,control.Height-120);
+
+        }
+    }
+
+
+}
