9f3e80676f2b8d84e95d77a513e933048e4c3ff0
Nikolai Merz <nikolai.merz@gmail.com>
2018-01-03 14:43:51 +0100

    Parser Klasse versucht zu erweitern --> Gescheitert

--- Reiner_Autoworker/WorkerClasses/AWDataParser.cs
+++ Reiner_Autoworker/WorkerClasses/AWDataParser.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+
 
 namespace Reiner_Autoworker.WorkerClasses
 {
     class PayPalParser
     {
+        public PayPalParser(string dataToParse)
+        {
+            Thread parsingThread = new Thread(pPPThread);
+            parsingThread.Start();
+        }
+
+        public void pPPThread()
+        {
+
+        }
+        
     }
+
+
 }
